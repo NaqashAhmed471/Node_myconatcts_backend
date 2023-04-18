a4913be33890e496f6838e781dafc9a2b5007d59
NaqashAhmed <naqash.ahmed@theowlsense.com>
2023-04-18 09:24:34 +0500

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,84 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+
+// Register the user
+// @Route Post /api/users/register
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already Registered!");
+  }
+
+  // Hash Password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("hashpassword", hashedPassword);
+
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(`User created ${user}`);
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
+});
+
+// Login user
+// @Route Post /api/users/login
+// @access public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+
+  console.log("comapre password", password, user.password);
+
+  // compare password with hashpassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password not valid");
+  }
+});
+
+// Current user information
+// @Route Post /api/users/current
+// @access private
+const currentUser = asyncHandler(async (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = { registerUser, loginUser, currentUser };
